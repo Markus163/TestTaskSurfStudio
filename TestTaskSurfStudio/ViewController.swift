395a5f6b40c94d30fef9472df97b4df4b4af39da
Mark Mikhailov <pprimak@mail.ru>
2023-02-09 19:56:23 +0200

    Initial Commit

---
+++ TestTaskSurfStudio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTaskSurfStudio
+//
+//  Created by Марк Михайлов on 09.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
